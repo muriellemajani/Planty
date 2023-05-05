93f5932037ac0d62e3235bda224c02acf37a2b3c
muriellemajani <murielle.majani@gmail.com>
2023-05-05 08:23:24 +0200

    Changement thème + ajout Footer & Header

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -1,9 +1,8 @@
 <?php
 
-// Action qui permet de charger des scripts dans notre thème
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles()
 {
-    // Chargement du style.css du thème parent Astra
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
